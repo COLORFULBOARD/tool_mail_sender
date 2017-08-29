306d2037421d05a19aa0ee52e7e4a727af8bba0e
rhoboro <rhoboro@gmail.com>
2017-08-29 15:53:47 +0900

    setup.pyを更新

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='mail_sender',
     version='0.1',
-    packages=find_packages(),
+    py_modules=['mail_sender'],
     install_requires=[]
 )
